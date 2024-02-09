b0842de7b6753881400171dd233bbc298e829844
syedkhasim15 <syedtahaseen123@gmail.com>
2024-02-09 17:03:39 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import './App.css';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter instead of createBrowserRouter
 import Index from './pages/Index';
 import Home from './components/home/Home';
 import Cart from './components/cart/Cart';
 import ProductDetail from './components/product/ProductDetail';
 
 function App() {
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route>
-          <Route path='/' element={<Index/>}>
-            <Route path='' element={<Home/>}/>
-            <Route path='cart' element={<Cart/>}/>
-            <Route path='product/:productId' element={<ProductDetail/>}/>
-          </Route>
-      
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}> {/* Set the basename here */}
+      <Routes>
+        <Route path="/" element={<Index />}>
+          <Route index element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="product/:productId" element={<ProductDetail />} />
         </Route>
-    )
-  )
-
-    return (
-      <>
-        
-          <RouterProvider router={router} />
-        
-      </>
-    
-    );
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
